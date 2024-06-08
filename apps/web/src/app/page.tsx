06bdc581496c9b55076c948dc4350631961c8727
Pedro Dias <pdr050203@gmail.com>
2024-06-07 23:10:11 -0300

    add: shadcnui to project

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,20 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="mt-24 flex flex-col items-center justify-center">
+      <div className="max-w-52 space-y-2">
+        <div className="flex flex-col justify-center gap-2">
+          <Label>Nome</Label>
+          <Input placeholder="Name" />
+        </div>
+
+        <Separator />
+        <Button>Sign In</Button>
+      </div>
+    </div>
+  )
 }
