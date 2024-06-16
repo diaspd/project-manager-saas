4ea44c8b7658035ebed97c1dc33b8bab17e2ba6f
Pedro Dias <pdr050203@gmail.com>
2024-06-16 03:05:10 -0300

    ci: get user permissions

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,14 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import Logo from '@/assets/logo.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability()
+
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
@@ -15,6 +18,8 @@ export function Header() {
         <Slash className="size-3 -rotate-[24deg] text-border" />
 
         <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>projects</p>}
       </div>
 
       <div className="flex items-center gap-4">
