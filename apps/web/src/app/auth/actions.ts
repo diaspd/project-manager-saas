fdc50c0ed10787e5d99e6c2f80d9a00cff299e86
Pedro Dias <pdr050203@gmail.com>
2024-06-15 23:22:44 -0300

    add: organization switcher component

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation'
 export async function signInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignInURL.searchParams.set('client_id', 'client_id')
+  githubSignInURL.searchParams.set('client_id', 'your_github_client_id')
   githubSignInURL.searchParams.set(
     'redirect_uri',
     'http://localhost:3000/api/auth/callback',
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,14 +1,20 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import Logo from '@/assets/logo.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
-        <Image src={Logo} className="size-6 dark:invert" alt="Logo" />
+        <Image src={Logo} className="size-5 dark:invert" alt="Logo" />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
