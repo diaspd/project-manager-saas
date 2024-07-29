36ed4b7a891c864d0f03dcdcc29b92bbdaac7a24
Pedro Dias <pdr050203@gmail.com>
2024-07-28 22:10:02 -0300

    feat: list invites

--- apps/web/src/app/(app)/org/[slug]/members/invites.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/invites.tsx
@@ -1,3 +1,69 @@
-export function Invites() {
-  return <div>Invites</div>
+import { ability, getCurrentOrg } from '@/auth/auth'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import { getInvites } from '@/http/get-invites'
+
+import { RevokeInviteButton } from './revoke-invite-button'
+
+export async function Invites() {
+  const currentOrg = getCurrentOrg()
+  const permissions = await ability()
+
+  const { invites } = await getInvites(currentOrg!)
+
+  return (
+    <div className="space-y-4">
+      {permissions?.can('create', 'Invite') && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Invite member</CardTitle>
+          </CardHeader>
+
+          <CardContent></CardContent>
+        </Card>
+      )}
+
+      <div className="space-y-2">
+        <h2 className="text-lg font-semibold">Members</h2>
+
+        <div className="rounded border">
+          <Table>
+            <TableBody>
+              {invites.map((invite) => {
+                return (
+                  <TableRow key={invite.id}>
+                    <TableCell className="py-2.5">
+                      <span className="text-muted-foreground">
+                        {invite.email}
+                      </span>
+                    </TableCell>
+                    <TableCell className="py-2.5 font-medium">
+                      <div className="w-fit rounded-md border px-1 ring-2 ring-muted-foreground">
+                        {invite.role}
+                      </div>
+                    </TableCell>
+                    <TableCell className="py-2.5">
+                      <div className="flex justify-end">
+                        {permissions?.can('delete', 'Invite') && (
+                          <RevokeInviteButton inviteId={invite.id} />
+                        )}
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+
+              {invites.length === 0 && (
+                <TableRow>
+                  <TableCell className="text-center text-muted-foreground">
+                    No invites found
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  )
 }
